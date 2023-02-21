1df7212709151460ba7fd34151aa123e59766897
Su <phuongta15099@gmail.com>
2023-02-21 10:19:02 +0700

    init

--- app/src/main/java/net/fpoly/dailymart/MainActivity.kt
+++ app/src/main/java/net/fpoly/dailymart/MainActivity.kt
@@ -1,7 +1,7 @@
 package net.fpoly.dailymart
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
