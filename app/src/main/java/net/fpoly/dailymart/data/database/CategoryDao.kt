afd9f4ec4a08248e68f298d775689465323d888f
Dattv <Dattv036@gmail.com>
2023-03-26 18:32:38 +0700

    category

--- app/src/main/java/net/fpoly/dailymart/data/database/CategoryDao.kt
+++ app/src/main/java/net/fpoly/dailymart/data/database/CategoryDao.kt
@@ -3,7 +3,7 @@ package net.fpoly.dailymart.data.database
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 import net.fpoly.dailymart.data.model.Category
-
+@Dao
 interface CategoryDao {
     /** get id */
     @Query("select * from category where id =:id")
