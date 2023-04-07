beee7d990ae0ef33c175f11b6c739be210872e5a
hienpv <hienpv@ikameglobal.com>
2023-04-07 10:31:27 +0700

    work sheet 1.2

--- app/src/main/java/net/fpoly/dailymart/view/work_sheet/SheetDayFragment.kt
+++ app/src/main/java/net/fpoly/dailymart/view/work_sheet/SheetDayFragment.kt
@@ -4,13 +4,31 @@ import androidx.fragment.app.activityViewModels
 import net.fpoly.dailymart.base.BaseFragment
 import net.fpoly.dailymart.databinding.FragmentDaySheetBinding
 
-class SheetDayFragment : BaseFragment<FragmentDaySheetBinding>(FragmentDaySheetBinding::inflate) {
+class SheetDayFragment(private val sheet: Sheet) :
+    BaseFragment<FragmentDaySheetBinding>(FragmentDaySheetBinding::inflate) {
 
     private val viewModel: WorkSheetViewModel by activityViewModels()
 
     override fun setupData() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+
+        val adapter1 = SheetAdapter(ArrayList())
+        val adapter2 = SheetAdapter(ArrayList())
+        val adapter3 = SheetAdapter(ArrayList())
+        binding.rcvCa1.adapter = adapter1
+        binding.rcvCa2.adapter = adapter2
+        binding.rcvCa3.adapter = adapter3
+        sheet.shift1?.let {
+            adapter1.setData(it)
+        }
+        sheet.shift2?.let {
+            adapter2.setData(it)
+        }
+        sheet.shift3?.let {
+            adapter3.setData(it)
+        }
+
     }
 
     override fun setupObserver() {
--- app/src/main/java/net/fpoly/dailymart/view/work_sheet/WorkSheetViewModel.kt
+++ app/src/main/java/net/fpoly/dailymart/view/work_sheet/WorkSheetViewModel.kt
@@ -3,4 +3,5 @@ package net.fpoly.dailymart.view.work_sheet
 import androidx.lifecycle.ViewModel
 
 class WorkSheetViewModel : ViewModel() {
+
 }
