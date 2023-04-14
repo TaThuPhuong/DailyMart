d64e9afa6ecb6dc28947eadd3a49a68feb2f31cb
TIEN <tienlsd123@gmail.com>
2023-04-15 00:30:37 +0700

    invoice, theme 14.4.2023

--- app/src/main/java/net/fpoly/dailymart/view/splash/SplashActivity.kt
+++ app/src/main/java/net/fpoly/dailymart/view/splash/SplashActivity.kt
@@ -2,10 +2,14 @@ package net.fpoly.dailymart.view.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import net.fpoly.dailymart.AppViewModelFactory
 import net.fpoly.dailymart.base.BaseActivity
 import net.fpoly.dailymart.data.model.User
@@ -18,20 +22,29 @@ import net.fpoly.dailymart.view.main.MainActivity
 import net.fpoly.dailymart.view.on_boarding.OnBoardingActivity
 
 @SuppressLint("CustomSplashScreen")
-class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
+class SplashActivity : AppCompatActivity() {
 
     private val viewModel: SplashViewModel by viewModels { AppViewModelFactory }
+    private lateinit var bindingData: ActivitySplashBinding
 
-    override fun setupData() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemGestures())
+            view.updatePadding(insets.left, 0, insets.right, insets.bottom)
+            view.updatePadding(0, 0, 0, 0)
+            WindowInsetsCompat.CONSUMED
+        }
+        bindingData = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(bindingData.root)
         viewModel.loadSplash()
         BankDao.getBankInfo {
             it?.let { bank ->
                 SharedPref.setBankInfo(this, bank)
             }
         }
-    }
 
-    override fun setupObserver() {
         viewModel.loadingSplash.observe(this) {
             if (it >= 200) {
                 if (SharedPref.getTimeOnApp(this) < 1) {
