0126fe0d1a299990160019d6528499693967b9a9
TIEN <tienlsd123@gmail.com>
2023-04-16 23:44:07 +0700

    theme,message1 16.4.2023

--- app/src/main/java/net/fpoly/dailymart/view/supplier/ConfirmRemoveDialog.kt
+++ app/src/main/java/net/fpoly/dailymart/view/supplier/ConfirmRemoveDialog.kt
@@ -1,14 +1,30 @@
 package net.fpoly.dailymart.view.supplier
 
+import android.app.Dialog
 import android.content.Context
+import android.os.Bundle
+import net.fpoly.dailymart.R
 import net.fpoly.dailymart.base.BaseDialog
 import net.fpoly.dailymart.data.model.Supplier
 import net.fpoly.dailymart.databinding.DialogRemoveConfirmBinding
 
-class ConfirmRemoveDialog(private val context: Context, private val supplier: Supplier, private val viewModel: SupplierViewModel) :
-    BaseDialog<DialogRemoveConfirmBinding>(context, DialogRemoveConfirmBinding::inflate) {
+class ConfirmRemoveDialog(
+    context: Context,
+    private val supplier: Supplier,
+    private val viewModel: SupplierViewModel
+) :
+    Dialog(context, R.style.BaseThemeDialog) {
 
-    override fun initData() {
+    private lateinit var binding: DialogRemoveConfirmBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DialogRemoveConfirmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initData()
+    }
+
+    fun initData() {
         binding.tvConfirm.setOnClickListener {
             viewModel.removeSupplier(supplier)
             dismiss()
